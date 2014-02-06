b468ab213e79dd754722656496e081337927655a
SJ Zhu <zsj950618@gmail.com>
2014-02-07 00:09:12 +0800

    new look

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from ustcmis import USTCMis
-from flask import Flask, session, redirect, url_for, request, json, current_app
+from flask import Flask, session, redirect, url_for, request, json,\
+                    current_app, render_template
 import random
 
 app = Flask(__name__)
@@ -10,15 +11,19 @@ url_prefix = ''
 
 @app.route(url_prefix + '/')
 def index():
-    if check_login():
-        return '''
-            <p>Logged in as %s</p>
-            <p><a href="%s">Grade</a></p>
-            <p><a href="%s">iCalendar file for 2014 Spring Semester</a></p>
-            <p><a href="%s">Logout</a></p>
-        ''' % (session['user'], url_for('api_get_grade'),
-        url_for('get_ical'), url_for('logout'))
-    return redirect(url_for('login'))
+    if 'id' in session:
+        user_id = session['id']
+    else:
+        user_id = str(int(random.random() * 1e16))
+        session['id'] = user_id
+    if user_id not in user:
+        user[user_id] = USTCMis()
+    login = user[user_id].check_login()
+    if not login:
+        img = user[user_id].get_check_code().encode('base64').replace('\n', '')
+    else:
+        img = ''
+    return render_template('index.html', login=login, img=img)
 
 
 @app.route(url_prefix + '/login', methods=['GET', 'POST'])
@@ -31,23 +36,7 @@ def login():
         pwd = request.form['pwd']
         check_code = request.form['check_code']
         user[user_id].login(user_code, pwd, check_code)
-        session['user'] = user_code
-        return redirect(url_for('index'))
-    if 'id' in session:
-        user_id = session['id']
-    else:
-        user_id = str(int(random.random() * 1e16))
-    user[user_id] = USTCMis()
-    session['id'] = user_id
-    return '''
-        <form action="" method="post">
-            <p>User ID<input type=text name=user_code></p>
-            <p>Password<input type=password name=pwd></p>
-            <p>Captcha<input type=text name=check_code>
-            <img src="data:image/jpeg;base64,%s"></p>
-            <p><input type=submit value=Login></p>
-        </form>
-    ''' % user[user_id].get_check_code().encode('base64').replace('\n', '')
+    return redirect(url_for('index'))
 
 
 @app.route(url_prefix + '/logout')
@@ -56,7 +45,7 @@ def logout():
         user_id = session['id']
         user.pop(user_id, None)
     session.clear()
-    return redirect(url_for('login'))
+    return redirect(url_for('index'))
 
 
 @app.route(url_prefix + '/ical.ics', methods=['GET', 'POST'])
@@ -70,11 +59,11 @@ def get_ical():
         icalendar = user[user_id].get_ical(semester)
         if icalendar != 'error':
             return current_app.response_class(icalendar,
-                mimetype='text/calendar')
+                                              mimetype='text/calendar')
     return json_return({'error': 'Not Login'})
 
 
-#API
+# API
 @app.route(url_prefix + '/api/login', methods=['GET', 'POST'])
 def api_login():
     if request.method == 'POST' and 'id' in session:
@@ -86,7 +75,6 @@ def api_login():
         check_code = request.form['check_code']
         status = user[user_id].login(user_code, pwd, check_code)
         if status:
-            session['user'] = user_code
             return json_return({'user': user_code})
         else:
             return json_return({'error': 'Login Failed'})
@@ -99,7 +87,7 @@ def api_login():
     img = user[user_id].get_check_code()
     return current_app.response_class(img, mimetype='image/jpeg')
     #img = user[user_id].get_check_code().encode('base64').replace('\n', '')
-    #return json_return({'img_base64': img})
+    # return json_return({'img_base64': img})
 
 
 @app.route(url_prefix + '/api/grade', methods=['GET', 'POST'])
@@ -129,13 +117,13 @@ def api_get_timetable():
 # else
 def json_return(dict_variable):
     return current_app.response_class(json.dumps(dict_variable,
-        indent=2, ensure_ascii=False),
-        mimetype='application/json')
+                    indent=2, ensure_ascii=False),
+                    content_type='application/json; charset=utf-8')
 
 
 def check_login():
-    return 'id' in session and 'user' in session and session['id'] in user
-        #and user[session['id']].check_login()
+    return 'id' in session and session['id'] in user \
+        and user[session['id']].check_login()
 
 
 # set the secret key.  keep this really secret:
