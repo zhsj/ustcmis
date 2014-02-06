ff4e2f41a4402bdaed73a20eaf255e47772a9d9d
SJ Zhu <zsj950618@gmail.com>
2014-02-06 14:03:04 +0800

    add timetable and ical

--- ustcmis.py
+++ ustcmis.py
@@ -1,10 +1,119 @@
 # -*- coding: utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
+from datetime import date, timedelta
+
+weeks_name = dict(zip(range(1, 8), ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU']))
+
+
+def decode_place_time(mess):
+    content = {}
+    mess = mess.split(';')
+    for item in mess:
+        item = item.split(u'：')
+        place = item[0]
+        if item[1].startswith(u'单') or item[1].startswith(u'双'):
+            other = item[1][0] + u'周'
+            week = item[1][1]
+        else:
+            other = ''
+            week = item[1][0]
+        if item[1].find(u'晚上') != -1:
+            classes = ['11']
+        else:
+            left = item[1].find('(')
+            right = item[1].find(')')
+            classes = item[1][left + 1: right].split(',')
+        for i in classes:
+            info = {
+                'place': place,
+                'week': week,
+                'class_no': i,
+                'other': other
+                }
+            content[len(content)] = info
+    return content
+
+
+def next_weekday(d, weekday):
+    days_ahead = weekday - d.weekday()
+    if days_ahead < 0:
+        days_ahead += 7
+    return d + timedelta(days_ahead)
+
+
+def ical(classes, class_day_time, semester_date, semester):
+    vcalendar = '''BEGIN:VCALENDAR
+PRODID:-//Google Inc//Google Calendar 70.9054//EN
+VERSION:2.0
+CALSCALE:GREGORIAN
+METHOD:PUBLISH
+BEGIN:VTIMEZONE
+TZID:Asia/Shanghai
+X-LIC-LOCATION:Asia/Shanghai
+BEGIN:STANDARD
+TZOFFSETFROM:+0800
+TZOFFSETTO:+0800
+TZNAME:CST
+DTSTART:19700101T000000
+END:STANDARD
+END:VTIMEZONE
+%s
+END:VCALENDAR
+'''
+    event = '''BEGIN:VEVENT
+DTSTART;TZID=Asia/Shanghai:%(class_firstday)sT%(class_start_time)s
+DTEND;TZID=Asia/Shanghai:%(class_firstday)sT%(class_end_time)s
+RRULE:FREQ=WEEKLY;UNTIL=%(semester_end_date)sT235900;BYDAY=%(class_week)s
+DESCRIPTION:%(class_info)s
+LOCATION:%(class_location)s
+SEQUENCE:0
+STATUS:CONFIRMED
+SUMMARY:%(class_name)s
+TRANSP:OPAQUE
+END:VEVENT
+'''
+    events = ''
+    for index, items in classes.iteritems():
+        for i, item in items[u'上课时间地点'].iteritems():
+            firstday = next_weekday(semester_date[semester][0],
+                                    int(item['week']) - 1).strftime('%Y%m%d')
+            info = {
+                'class_firstday': firstday,
+                'class_start_time': class_day_time[item['class_no']][0],
+                'class_end_time': class_day_time[item['class_no']][1],
+                'semester_end_date':
+                    semester_date[semester][1].strftime('%Y%m%d'),
+                'class_week': weeks_name[int(item['week'])],
+                'class_info': ' '.join([items[u'教师'], item['other']]),
+                'class_location': item['place'],
+                'class_name': items[u'课程名称']
+                }
+            events += event % info
+    vcalendar = vcalendar % events
+    return vcalendar
 
 
 class USTCMis:
     url = 'http://mis.teach.ustc.edu.cn/'
+    class_day_time = {
+        '1': ['075000', '083500'],
+        '2': ['084000', '092500'],
+        '3': ['094500', '103000'],
+        '4': ['103500', '112000'],
+        '5': ['112500', '121000'],
+        '6': ['140000', '144500'],
+        '7': ['145000', '153500'],
+        '8': ['155500', '164000'],
+        '9': ['164500', '173000'],
+        '10': ['173500', '182000'],
+        '11': ['193000', '201500'],
+        '12': ['202000', '210500'],
+        '13': ['211000', '215500']
+        }
+    semester_date = {
+        '20132': [date(2014, 2, 17), date(2014, 6, 20)]
+        }
 
     def __init__(self):
         self.s = requests.Session()
@@ -32,9 +141,9 @@ class USTCMis:
         self.login_status = (r.text.find(u'所在院系') != -1)
         return self.login_status
 
-    def get_grade(self, semester):
+    def get_grade(self, semester=''):
         if not self.check_login():
-            return "login error"
+            return {'error': 'Not Login'}
         query_data = {
             'xuenian': semester,
             'px': 1,
@@ -55,3 +164,27 @@ class USTCMis:
             detail[i] = dict(zip(detail_key, item))
         content['detail'] = detail
         return content
+
+    def get_timetable(self, semester):
+        if not self.check_login():
+            return {'error': 'Not Login'}
+        query_data = {'selxnxq': semester}
+        r = self.s.post(USTCMis.url + 'xkjgquery.do?tjfs=1', data=query_data)
+        soup = BeautifulSoup(r.text)
+        table = soup.find_all('table', id='jcxxtable0')[0]
+        tds = [i.string.strip() for i in table.find_all('td')]
+        info_key = tds[0:8]
+        info_item = [tds[i:i + 8] for i in xrange(8, len(tds), 8)]
+        classes = [dict(zip(info_key, item)) for item in info_item]
+        content = dict(zip(range(len(info_item)), classes))
+        for i in content.keys():
+            mess = content[i][u'上课时间地点']
+            content[i][u'上课时间地点'] = decode_place_time(mess)
+        return content
+
+    def get_ical(self, semester='20132'):
+        if not self.check_login():
+            return 'error'
+        classes = self.get_timetable(semester)
+        return ical(classes, USTCMis.class_day_time, USTCMis.semester_date,
+                    semester)
--- web.py
+++ web.py
@@ -14,8 +14,10 @@ def index():
         return '''
             <p>Logged in as %s</p>
             <p><a href="%s">Grade</a></p>
+            <p><a href="%s">Timetable</a></p>
             <p><a href="%s">Logout</a></p>
-        ''' % (session['user'], url_for('api_get_grade'), url_for('logout'))
+        ''' % (session['user'], url_for('api_get_grade'),
+        url_for('api_get_timetable'), url_for('logout'))
     return redirect(url_for('login'))
 
 
@@ -57,12 +59,46 @@ def logout():
     return redirect(url_for('login'))
 
 
-def check_login():
-    return 'id' in session and 'user' in session and \
-        session['id'] in user and user[session['id']].check_login()
+@app.route(url_prefix + '/ical.ics')
+def get_ical():
+    if check_login():
+        user_id = session['id']
+        icalendar = user[user_id].get_ical()
+        if icalendar != 'error':
+            return current_app.response_class(icalendar,
+                mimetype='text/calendar')
+    return json_return({'error': 'Not Login'})
 
 
-@app.route(url_prefix + '/grade', methods=['GET', 'POST'])
+#API
+@app.route(url_prefix + '/api/login', methods=['GET', 'POST'])
+def api_login():
+    if request.method == 'POST' and 'id' in session:
+        user_id = session['id']
+        if user_id not in user:
+            return redirect(url_for('login'))
+        user_code = request.form['user_code']
+        pwd = request.form['pwd']
+        check_code = request.form['check_code']
+        status = user[user_id].login(user_code, pwd, check_code)
+        if status:
+            session['user'] = user_code
+            return json_return({'user': user_code})
+        else:
+            return json_return({'error': 'Login Failed'})
+    if 'id' in session:
+        user_id = session['id']
+    else:
+        user_id = str(int(random.random() * 1e16))
+    user[user_id] = USTCMis()
+    session['id'] = user_id
+    img = user[user_id].get_check_code()
+    return current_app.response_class(img, mimetype='image/jpeg')
+    #img = user[user_id].get_check_code().encode('base64').replace('\n', '')
+    #return json_return({'img_base64': img})
+
+
+@app.route(url_prefix + '/api/grade', methods=['GET', 'POST'])
 def api_get_grade():
     if check_login():
         try:
@@ -74,10 +110,30 @@ def api_get_grade():
     return json_return({'error': 'Not Login'})
 
 
+@app.route(url_prefix + '/api/timetable', methods=['GET', 'POST'])
+def api_get_timetable():
+    if check_login():
+        try:
+            semester = request.values['semester']
+        except:
+            semester = '20131'
+        user_id = session['id']
+        return json_return(user[user_id].get_timetable(semester))
+    return json_return({'error': 'Not Login'})
+
+
+# else
 def json_return(dict_variable):
     return current_app.response_class(json.dumps(dict_variable,
         indent=2, ensure_ascii=False),
         mimetype='application/json')
+
+
+def check_login():
+    return 'id' in session and 'user' in session and session['id'] in user
+        #and user[session['id']].check_login()
+
+
 # set the secret key.  keep this really secret:
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
